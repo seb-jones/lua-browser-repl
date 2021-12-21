8ac7d6b10875e21907e53f48a355c66928547ddb
Seb Jones <jones.seb95@googlemail.com>
2021-12-21 20:23:23 +0000

    Execute inputs as return statements

--- code/main.c
+++ code/main.c
@@ -7,6 +7,29 @@
 
 lua_State *L = NULL;
 
+/* assumes an error message is on the top of the stack */
+void print_error()
+{
+    fprintf(stderr, "%s\n", lua_tostring(L, -1));
+
+    lua_pop(L, 1);
+}
+
+void print_values_on_stack()
+{
+    int n = lua_gettop(L);
+
+    if (n > 0) {
+        luaL_checkstack(L, LUA_MINSTACK, "too many results to print");
+
+        lua_getglobal(L, "print");
+
+        lua_insert(L, 1);
+
+        if (lua_pcall(L, n, 0, 0) != LUA_OK) print_error();
+    }
+}
+
 void initialise_lua()
 {
     L = luaL_newstate();
@@ -20,10 +43,13 @@ void initialise_lua()
 
 void parse(char *input)
 {
-    int error = luaL_loadstring(L, input) || lua_pcall(L, 0, 0, 0);
+    const char *s = lua_pushfstring(L, "return %s", input);
 
-    if (error) {
-        fprintf(stderr, "%s\n", lua_tostring(L, -1));
-        lua_pop(L, 1);
-    }
+    luaL_loadstring(L, s);
+
+    lua_remove(L, -2);
+
+    if (lua_pcall(L, 0, LUA_MULTRET, 0)) print_error();
+
+    print_values_on_stack();
 }
