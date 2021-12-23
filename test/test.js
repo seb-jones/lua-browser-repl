ad5fc58f79bf7895d65cc89e6192b918f82b675a
Seb Jones <jones.seb95@googlemail.com>
2021-12-23 00:00:34 +0000

    Use cwrap instead of ccall in unit tests

--- test/test.js
+++ test/test.js
@@ -12,46 +12,33 @@ describe("Lua REPL Module", function() {
             stderr = [];
 
             var Module = {
-                'print': function(text) { stdout.push(text) },
-                'printErr': function(text) { stderr.push(text) }
+                "print": function(text) { stdout.push(text) },
+                "printErr": function(text) { stderr.push(text) }
             };
 
             this.instance = await moduleFactory(Module);
 
             this.instance.ccall("initialise_lua");
+
+            this.parse = this.instance.cwrap("parse", "number", [ "string" ]);
         });
 
         it("Evaluates a Lua expression", function() {
-            this.instance.ccall(
-                "parse",
-                "number",
-                [ "string" ],
-                [ "3 + 3" ]
-            );
+            this.parse("3 + 3");
 
             expect(stdout.length).to.equal(2);
-            expect(stdout[1]).to.equal('6');
+            expect(stdout[1]).to.equal("6");
         });
 
         it("Evaluates a Lua assignment", function() {
-            this.instance.ccall(
-                "parse",
-                "number",
-                [ "string" ],
-                [ "x = 10" ]
-            );
+            this.parse("x = 10");
 
             expect(stdout.length).to.equal(1);
 
-            this.instance.ccall(
-                "parse",
-                "number",
-                [ "string" ],
-                [ "x" ]
-            );
+            this.parse("x");
 
             expect(stdout.length).to.equal(2);
-            expect(stdout[1]).to.equal('10');
+            expect(stdout[1]).to.equal("10");
         });
     });
 });
