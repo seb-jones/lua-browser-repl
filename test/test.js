a42f683715d3d326d1f2be6997c59b97d2342364
Seb Jones <jones.seb95@googlemail.com>
2021-12-29 19:00:16 +0000

    Refactor unit tests a little

--- test/test.js
+++ test/test.js
@@ -2,27 +2,29 @@ const { expect } = require("chai");
 
 const createModule = require("../build/compiled.js");
 
+const welcomeMessage = "Welcome! This REPL is using Lua 5.4";
+
 let stdout = [];
 let stderr = [];
 
 describe("Lua REPL Module", function() {
+    beforeEach(async function() {
+        stdout = [];
+        stderr = [];
+
+        const options = {
+            "print": function(text) { stdout.push(text) },
+            "printErr": function(text) { stderr.push(text) }
+        };
+
+        this.instance = await createModule(options);
+
+        this.instance.ccall("initialise_lua");
+
+        this.parse = this.instance.cwrap("parse", "number", [ "string" ]);
+    });
+
     describe("#parse", function() {
-        beforeEach(async function() {
-            stdout = [];
-            stderr = [];
-
-            const options = {
-                "print": function(text) { stdout.push(text) },
-                "printErr": function(text) { stderr.push(text) }
-            };
-
-            this.instance = await createModule(options);
-
-            this.instance.ccall("initialise_lua");
-
-            this.parse = this.instance.cwrap("parse", "number", [ "string" ]);
-        });
-
         it("Evaluates a Lua expression", function() {
             this.parse("3 + 3");
 
@@ -48,9 +50,7 @@ describe("Lua REPL Module", function() {
 
             expect(stdout).to.be.an('array')
                 .with.lengthOf(1)
-                .and.members([
-                    "Welcome! This REPL is using Lua 5.4"
-                ]);
+                .and.members([ welcomeMessage ]);
         });
 
         it("Writes errors to stderr", function() {
@@ -58,9 +58,7 @@ describe("Lua REPL Module", function() {
 
             expect(stdout).to.be.an('array')
                 .with.lengthOf(1)
-                .and.members([
-                    "Welcome! This REPL is using Lua 5.4"
-                ]);
+                .and.members([ welcomeMessage ]);
 
             expect(stderr).to.be.an('array')
                 .with.lengthOf(1)
